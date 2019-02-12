6413fbeb096e34fdb040dbabfcf5578a0b4a52ca
LLipter <katsudonn@163.com>
2019-02-11 22:18:50 -0800

    q3 done

--- proj2/multiAgents.py
+++ proj2/multiAgents.py
@@ -150,7 +150,6 @@ class MinimaxAgent(MultiAgentSearchAgent):
         Returns whether or not the game state is a losing state
         """
         "*** YOUR CODE HERE ***"
-        # print(self.depth)
         _, action = self.getMaxNodeScore(gameState, 1)
 
         return action
@@ -195,7 +194,44 @@ class AlphaBetaAgent(MultiAgentSearchAgent):
         Returns the minimax action using self.depth and self.evaluationFunction
         """
         "*** YOUR CODE HERE ***"
-        util.raiseNotDefined()
+        _, action = self.getMaxNodeScore(gameState, 1, float('-inf'), float('inf'))
+
+        return action
+
+    def getMaxNodeScore(self, gameState, depth, alpha, beta):
+        max_score = float('-inf')
+        max_action = Directions.STOP
+        for action in gameState.getLegalActions(0):
+            successor = gameState.generateSuccessor(0, action)
+            score = self.getMinNodeScore(successor, 1, depth, alpha, beta)
+            if score > beta:
+                return score, action
+            alpha = max(alpha, score)
+            if score > max_score:
+                max_score = score
+                max_action = action
+        if gameState.isLose() or gameState.isWin():
+            max_score = self.evaluationFunction(gameState)
+        return max_score, max_action
+
+    def getMinNodeScore(self, gameState, index, depth, alpha, beta):
+        min_score = float('inf')
+        for action in gameState.getLegalActions(index):
+            successor = gameState.generateSuccessor(index, action)
+            if index < gameState.getNumAgents() - 1:
+                score = self.getMinNodeScore(successor, index + 1, depth, alpha, beta)
+            elif depth < self.depth:
+                score, _ = self.getMaxNodeScore(successor, depth + 1, alpha, beta)
+            else:
+                score = self.evaluationFunction(successor)
+            if score < alpha:
+                return score
+            beta = min(beta, score)
+            if score < min_score:
+                min_score = score
+        if gameState.isLose() or gameState.isWin():
+            min_score = self.evaluationFunction(gameState)
+        return min_score
 
 
 class ExpectimaxAgent(MultiAgentSearchAgent):
