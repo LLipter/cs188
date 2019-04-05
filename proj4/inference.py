bc534decffb7079ff41c40581bc6d32a8b209ccf
LLipter <katsudonn@163.com>
2019-04-05 11:20:21 -0700

    q10 done

--- proj4/inference.py
+++ proj4/inference.py
@@ -522,8 +522,9 @@ class JointParticleFilter(ParticleFilter):
 
             # now loop through and update each entry in newParticle...
             "*** YOUR CODE HERE ***"
-            raiseNotDefined()
-
+            for i in range(self.numGhosts):
+                newPosDist = self.getPositionDistribution(gameState, oldParticle, i, self.ghostAgents[i])
+                newParticle[i] = newPosDist.sample()
             """*** END YOUR CODE HERE ***"""
             newParticles.append(tuple(newParticle))
         self.particles = newParticles
