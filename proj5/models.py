12ccbcf51b3163acd5b72b5b93159acc360ae232
LLipter <katsudonn@163.com>
2019-04-17 22:40:43 -0700

    q3 check point

--- proj5/models.py
+++ proj5/models.py
@@ -106,7 +106,7 @@ class RegressionModel(object):
         """
         "*** YOUR CODE HERE ***"
         y_pred = self.run(x)
-        loss = nn.SquareLoss(y, y_pred)
+        loss = nn.SquareLoss(y_pred, y)
         return loss
 
     def train(self, dataset):
@@ -134,7 +134,6 @@ class RegressionModel(object):
                 average_loss = loss_sum / lost_length
                 if cnt % print_interval == 0:
                     print("average_loss:", average_loss)
-                # print("lost_window:", lost_window)
                 if average_loss <= 0.015:
                     break
             grad_wrt_w1, grad_wrt_w2, grad_wrt_w3, grad_wrt_b1, grad_wrt_b2, grad_wrt_b3 = nn.gradients(loss,
@@ -170,6 +169,14 @@ class DigitClassificationModel(object):
     def __init__(self):
         # Initialize your model parameters here
         "*** YOUR CODE HERE ***"
+        self.w1 = nn.Parameter(784, 100)
+        self.b1 = nn.Parameter(1, 100)
+        self.w2 = nn.Parameter(100, 100)
+        self.b2 = nn.Parameter(1, 100)
+        self.w3 = nn.Parameter(100, 10)
+        self.b3 = nn.Parameter(1, 10)
+        self.batch_size = 100
+        self.lr = 0.005
 
     def run(self, x):
         """
@@ -186,6 +193,10 @@ class DigitClassificationModel(object):
                 (also called logits)
         """
         "*** YOUR CODE HERE ***"
+        h1 = nn.ReLU(nn.AddBias(nn.Linear(x, self.w1), self.b1))
+        h2 = nn.ReLU(nn.AddBias(nn.Linear(h1, self.w2), self.b2))
+        y_pred = nn.AddBias(nn.Linear(h2, self.w3), self.b3)
+        return y_pred
 
     def get_loss(self, x, y):
         """
@@ -201,12 +212,35 @@ class DigitClassificationModel(object):
         Returns: a loss node
         """
         "*** YOUR CODE HERE ***"
+        y_pred = self.run(x)
+        loss = nn.SoftmaxLoss(y_pred, y)
+        return loss
 
     def train(self, dataset):
         """
         Trains the model.
         """
         "*** YOUR CODE HERE ***"
+        while True:
+            for x, y in dataset.iterate_once(self.batch_size):
+                loss = self.get_loss(x, y)
+                grad_wrt_w1, grad_wrt_w2, grad_wrt_w3, grad_wrt_b1, grad_wrt_b2, grad_wrt_b3 = nn.gradients(loss,
+                                                                                                            [self.w1,
+                                                                                                             self.w2,
+                                                                                                             self.w3,
+                                                                                                             self.b1,
+                                                                                                             self.b2,
+                                                                                                             self.b3])
+                self.w1.update(grad_wrt_w1, - self.lr)
+                self.w2.update(grad_wrt_w2, - self.lr)
+                self.w3.update(grad_wrt_w3, - self.lr)
+                self.b1.update(grad_wrt_b1, - self.lr)
+                self.b2.update(grad_wrt_b2, - self.lr)
+                self.b3.update(grad_wrt_b3, - self.lr)
+            acc = dataset.get_validation_accuracy()
+            print("acc:", acc)
+            if acc >= 0.98:
+                break
 
 
 class LanguageIDModel(object):
